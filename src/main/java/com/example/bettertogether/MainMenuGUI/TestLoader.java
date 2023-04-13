86bac60be36970114b54398034216b208b3ba331
szlazakm <michal.szlazak77@gmail.com>
2023-04-13 13:39:05 +0200

    Changed TestLoader Class implementation.
    Changed TestLoader Class implementation -> removed loader code from
    constructor to a public method.

--- src/main/java/com/example/bettertogether/MainMenuGUI/TestLoader.java
+++ src/main/java/com/example/bettertogether/MainMenuGUI/TestLoader.java
@@ -8,10 +8,16 @@ import java.io.File;
 public class TestLoader {
 
     private final String pathToTestFolder = FolderPaths.pathToTestFolder;
+    private ListView<String> testListView;
 
     public TestLoader(ListView<String> testListView) {
+        this.testListView = testListView;
+    }
+
+    public void load() {
         File testDir = new File(pathToTestFolder);
         File[] testFiles = testDir.listFiles();
+
         if(testFiles == null) {
             return;
         }
