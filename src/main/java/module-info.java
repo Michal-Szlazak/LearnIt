b594c09752a61f621acfca2cd745a288ca0d235e
Szlązak Michał (STUD) <01169634@pw.edu.pl>
2023-04-15 14:16:56 +0200

    Changed json package export in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,4 +11,6 @@ module com.example.bettertogether {
     exports com.example.bettertogether;
     exports com.example.bettertogether.Test;
     opens com.example.bettertogether.Test to javafx.fxml;
+    exports com.example.bettertogether.JsonMappers;
+    opens com.example.bettertogether.JsonMappers to javafx.fxml;
 }
